194feeb9d1a03330eff67eea8c812842760e464f
Onesmus Wambugu <onesmus1024@gmail.com>
2024-01-16 17:48:43 +0300

    feat: add pop,append and print_list to the linkedlist

--- data structure/linear/linkedlist/linkedlist.py
+++ data structure/linear/linkedlist/linkedlist.py
@@ -10,6 +10,82 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+    
+    def  print_list(self):
+        temp = self.head
+
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+
+    def append(self,value):
+
+        if self.head == None:
+            new_node = Node(value)
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node = Node(value)
+            self.tail.next = new_node
+            self.tail = new_node
+            self.length +=1
+
+    def pop(self):
+        if self.head == None:
+            print("The list is empty")
+        previous = self.head
+        temp = self.head
+
+        while temp.next is not None:
+            previous = temp
+            temp = temp.next
+        self.tail = previous
+        self.tail.next = None
+        self.length -=1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        
+
+
+            return temp.value
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    towns_to_visit = LinkedList("Nyeri")
+    print("New List")
+    towns_to_visit.print_list()
+
+    # Add a new town
+    print("Add New Node")
+    towns_to_visit.append("Nanyuki")
+
+    # Get All Towns
+
+    towns_to_visit.print_list()
+
+    #Remove the last town
+    print("Remove One Node")
+    towns_to_visit.pop()
+    towns_to_visit.pop()
+
+    # Get towns
+    towns_to_visit.print_list()
+    
+    
+
 
 
         
